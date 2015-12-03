ea50c035dc670c5d67c2a72c123aa78f06432981
snackattas <zach.attas@gmail.com>
2015-12-03 16:21:04 -0600

    Added more notes, fixed magic variables, removed logging module, switched tab to spaces

--- Stage4Intro.py
+++ Stage4Intro.py
@@ -3,11 +3,10 @@ import jinja2
 import os
 import time
 from google.appengine.ext import ndb
-import logging
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                                                                        autoescape = True)
+                                                                autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -26,8 +25,9 @@ class MainPage(Handler):
         data = Help.get_data()
         [disableLesson, disableConcept, disableInformation] = Help.radio_buttons()
         self.render('template.html', data = data, disableLesson = disableLesson, 
-                   disableConcept = disableConcept, disableInformation = disableInformation, 
+                    disableConcept = disableConcept, disableInformation = disableInformation, 
                     error = error)
+
 class ErrorPage(Handler):
     def get(self):
         error = 1
@@ -36,8 +36,9 @@ class ErrorPage(Handler):
         data = Help.get_data()
         [disableLesson, disableConcept, disableInformation] = Help.radio_buttons()
         self.render('template.html', data = data, disableLesson = disableLesson, 
-                   disableConcept = disableConcept, disableInformation = disableInformation, 
+                    disableConcept = disableConcept, disableInformation = disableInformation, 
                     error = error)
+
 class Submit(Handler):
     def post(self):
         note_text = self.request.get("note_text")
@@ -47,10 +48,12 @@ class Submit(Handler):
             ID.note_text = note_text
             ID.type_of_note = type_of_note
             ID.put()
-            time.sleep(1)
+            delay = 1 #  1 second
+            time.sleep(delay)
             self.redirect('/')
         else:
             self.redirect('/ErrorPage')
+
 class Helper:
     def get_data(self):
         return Note.query().order(Note.date)
@@ -58,21 +61,23 @@ class Helper:
         most_recent_entry = 1
         qry = Note.query().order(-Note.date).fetch(most_recent_entry)
         if qry:
-          type_of_note = qry[0].type_of_note
-          if type_of_note == 'Lesson':
-              return ['disabled', '', 'disabled']
-          if type_of_note == 'Concept':
-              return ['disabled', 'disabled', '']
-          else:
-              return ['', '', '']
+            type_of_note = qry[0].type_of_note
+            if type_of_note == 'Lesson':
+                return ['disabled', '', 'disabled']
+            if type_of_note == 'Concept':
+                return ['disabled', 'disabled', '']
+            else:
+                return ['', '', '']
         else:
             return ['','disabled','disabled']
+
 class Note(ndb.Model):
     type_of_note = ndb.StringProperty()
     note_text = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
+
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/ErrorPage', ErrorPage),
-															('/Submit', Submit)],
-															 debug=True)
+                              ('/ErrorPage', ErrorPage),
+                              ('/Submit', Submit)],
+                              debug=True)
 
